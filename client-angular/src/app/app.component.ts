eca8bdf7a9bffe34fb3c28429a7f49fd927c0c9d
loom <shirconita@gmail.com>
2022-06-26 17:24:07 +0300

    class

--- client-angular/src/app/app.component.ts
+++ client-angular/src/app/app.component.ts
@@ -6,21 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Welcome';
+  
   inputType ='text';
-  success =true;
+ 
   night = true;
   items = ['orange', 'peach', 'watermelon'];
-  labels =['Customers', 'Produscts', 'Orders'];
+  
 
-  getTitleClass():string {
-    return this.success ? 'text-info' : 'text-danger';
-  }
 
-  toggleTitleColor(){
-    this.success = !this.success
-    
-  }
+
+ 
 
   isItNight(){
     return this.night
--- client-angular/src/app/home/home.component.ts
+++ client-angular/src/app/home/home.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  labels =['Customers', 'Produscts', 'Orders'];
+  title = 'Welcome';
+  success =true;
+
+  toggleTitleColor(){
+    this.success = !this.success
+    
+  }
+
+  getTitleClass():string {
+    return this.success ? 'text-info' : 'text-danger';
+    
+  }
+
   constructor() { }
 
   ngOnInit(): void {
