e74a9398588fd02069de329536352ce84768fad2
loom <shirconita@gmail.com>
2022-07-12 19:30:59 +0300

    Yes

--- app.js
+++ app.js
@@ -5,14 +5,16 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 
-const auth = require('./middleware/auth')
+const auth = require('./middleware/auth');
+const headers = require('./middleware/headers');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var customersRouter = require('./routes/customers');
 var productsRouter = require('./routes/products')
 const products = require('./controllers/products');
-const customers = require ('./controllers/customers')
+const customers = require ('./controllers/customers');
+var countriesRouter = require('./routes/countries');
 
 var app = express();
 
@@ -21,13 +23,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'exports')));
 
-
+app.use(headers);
 app.use('/', indexRouter);
 app.use('/users', auth, usersRouter);
 app.use('/customers', auth, customersRouter);
 app.use('/products', auth, productsRouter);
 //app.use('/orders', auth, ordersRouter);
+app.use('/countries', auth, countriesRouter);
 
+// catch 404 err forward error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+})
+
+// custom error handler
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.locals.message = err.message;
+    res.locals.error = err;
+
+    res.status(500).send(err);
+})
 
 module.exports = app;
